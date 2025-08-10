d57929b5438e32caa62398185596ee60730431df
Luigizu <luigizuin0@gmail.com>
2025-08-10 00:49:38 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 // ATENCIÓN: Pega aquí la URL de la aplicación web que obtuviste de Google Apps Script.
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbyWTOZD9eFyKKYIhoHJunuLnTKJdxudeRDp-GZgsCnh78Q-WuN9VYhczWOt8B5yGmS4Mg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycby1gVZQjRT5vmdjBJIcvjckf41wuuVDIDttnW9O10Ok0iWkFFn0OD1OMIdVkaWh02Xf6w/exec';
+
 
